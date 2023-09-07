6355ba0a2830b1113569a6e99aece6e5241f8568
B-Bhanu-Teja <bbt1203@gmail.com>
2023-09-08 02:12:25 +0530

    changed offboard_sub.py

--- src/planner/plan_manage/scripts/offboard_sub.py
+++ src/planner/plan_manage/scripts/offboard_sub.py
@@ -4,11 +4,14 @@ import rospy
 
 # 3D point & Stamped Pose msgs
 from geometry_msgs.msg import Point, PoseStamped
-from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist, TwistStamped
+from geometry_msgs.msg import Vector3, Vector3Stamped
+
 # import all mavros messages and services
 from mavros_msgs.msg import *
 from mavros_msgs.srv import *
 import numpy as np
+from nav_msgs.msg import Odometry
 
 # Flight modes class
 # Flight modes are activated using ROS services
@@ -86,9 +89,13 @@ class Controller:
         # Drone state
         self.state = State()
         # Instantiate a setpoints message
-        self.sp = Odometry()
+        self.sp = PoseStamped()
         # set the flag to use position setpoints and yaw angle
-        
+        self.sv = TwistStamped()
+        self.sa = Vector3Stamped()
+
+        # self.sodomcmd = Odometry()
+
         # Step size for position update
         self.STEP_SIZE = 2.0
         # Fence. We will assume a square fence for now
@@ -96,7 +103,6 @@ class Controller:
 
         # A Message for the current local position of the drone
         
-
         # initial values for setpoints
         self.sp.pose.position.x = 0.0
         self.sp.pose.position.y = 0.0
@@ -104,8 +110,29 @@ class Controller:
         self.sp.pose.position.z = self.ALT_SP
         self.local_pos = Point(0.0, 0.0, self.ALT_SP)
 
+        self.sv.twist.linear
+
         # speed of the drone is set using MPC_XY_CRUISE parameter in MAVLink
         # using QGroundControl. By default it is 5 m/s.
+
+    def get_quaternion_from_euler(self, roll, pitch, yaw):
+        """
+        Convert an Euler angle to a quaternion.
+        
+        Input
+            :param roll: The roll (rotation around x-axis) angle in radians.
+            :param pitch: The pitch (rotation around y-axis) angle in radians.
+            :param yaw: The yaw (rotation around z-axis) angle in radians.
+        
+        Output
+            :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
+        """
+        self.qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        self.qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+        self.qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+        self.qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        
+        return [self.qx, self.qy, self.qz, self.qw]
 
     # Callbacks
 
@@ -115,15 +142,31 @@ class Controller:
         self.local_pos.y = msg.pose.position.y
         self.local_pos.z = msg.pose.position.z
 
-    def newPoseCB(self, msg):
-        self.sp.pose.position.x = msg.pose.position.x
-        self.sp.pose.position.y = msg.pose.position.y
-        self.sp.pose.position.z = msg.pose.position.z
+    # def newPoseCB(self, msg):
+    #     self.sp.pose.position.x = msg.pose.position.x
+    #     self.sp.pose.position.y = msg.pose.position.y
+    #     self.sp.pose.position.z = msg.pose.position.z
     
-        self.sp.pose.orientation.x = msg.pose.orientation.x
-        self.sp.pose.orientation.y = msg.pose.orientation.y
-        self.sp.pose.orientation.z = msg.pose.orientation.z
-        self.sp.pose.orientation.w = msg.pose.orientation.w
+    #     self.sp.pose.orientation.x = msg.pose.orientation.x
+    #     self.sp.pose.orientation.y = msg.pose.orientation.y
+    #     self.sp.pose.orientation.z = msg.pose.orientation.z
+    #     self.sp.pose.orientation.w = msg.pose.orientation.w
+    
+    def newPoseOdomCmdCB(self, msg):
+        self.sp.pose.position.x = msg.pose.pose.position.x
+        self.sp.pose.position.y = msg.pose.pose.position.y
+        self.sp.pose.position.z = msg.pose.pose.position.z
+    
+        self.sv.twist.linear.x = msg.twist.twist.linear.x
+        self.sv.twist.linear.y = msg.twist.twist.linear.y
+        self.sv.twist.linear.z = msg.twist.twist.linear.z       
+
+        self.sa.vector.x = msg.twist.twist.angular.x
+        self.sa.vector.y = msg.twist.twist.angular.y
+        self.sa.vector.z = msg.twist.twist.angular.z
+
+        self.sp.pose.orientation = self.get_quaternion_from_euler(0,0,msg.pose.pose.orientation.x)
+        self.sv.twist.angular.z = msg.pose.pose.orientation.y
 
     ## Drone State callback
     def stateCb(self, msg):
@@ -146,10 +189,11 @@ def main():
     # Subscribe to drone's local position
     # rospy.Subscriber('mavros/local_position/pose', PoseStamped, cnt.posCb)
 
-    rospy.Subscriber('/position_odom_cmd', Odometry, cnt.newPoseCB) #new waypoint
-
-    sp_pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)
-
+    # rospy.Subscriber('new_pose', PoseStamped, cnt.newPoseCB) #new waypoint
+    rospy.Subscriber("/position_odom_cmd", Odometry, cnt.newPoseOdomCmdCB)
+    sp_pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size=10)
+    sv_pub = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=10)
+    sa_pub = rospy.Publisher('/mavros/setpoint_accel/accel', Vector3Stamped, queue_size=10)
     # Setpoint publisher    
     movement_cmd = AttitudeTarget()
     thrust_pub = rospy.Publisher('mavros/setpoint_raw/attitude', AttitudeTarget, queue_size=1)
@@ -171,6 +215,8 @@ def main():
     k=0
     while k<20:
         sp_pub.publish(cnt.sp)
+        sv_pub.publish(cnt.sv)
+        sa_pub.publish(cnt.sa)
         rate.sleep()
         k = k + 1
 
@@ -183,6 +229,8 @@ def main():
     while not rospy.is_shutdown():
 
         sp_pub.publish(cnt.sp)    
+        sv_pub.publish(cnt.sv)
+        sa_pub.publish(cnt.sa)
         rate.sleep()
         print("while loop")
         
